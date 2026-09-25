e42c1df2ca426eda04f58948bcdf9707c8074e3e
Manash Prasad <manashprasad94@gmail.com>
2026-09-26 01:59:52 +0530

    Changes

--- antarctic-navigation-backend/app/main.py
+++ antarctic-navigation-backend/app/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_origins=[
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://antartica-ice-git-main-manashprasad94-9469s-projects.vercel.app/"
+    "https://antartica-ice-git-main-manashprasad94-9469s-projects.vercel.app"
 ],
     allow_credentials=True,
     allow_methods=["*"],
