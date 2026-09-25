58a0855365c8698039e2de5b60594b53d128acd9
Manash Prasad <manashprasad94@gmail.com>
2026-09-26 01:50:16 +0530

    Changes

--- antarctic-navigation-backend/app/main.py
+++ antarctic-navigation-backend/app/main.py
@@ -22,8 +22,8 @@ app.add_middleware(
     allow_origins=[
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://antartica-ice.onrender.com",
-]
+    "https://antartica-ice.onrender.com"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
